5cd268ce6f66a56db114400989e8b1083f1ff062
qbxt <10813JJ@gmail.com>
2021-06-19 06:46:03 -0600

    make copyCard click-to-copy

--- components/copyCard.js
+++ components/copyCard.js
@@ -6,11 +6,21 @@ import textStyles from '../styles/text.module.css'
 export function CopyCard({title, content}) {
     return (
         <>
-            <div className={cardStyles.containerShadowWrapper}>
+            <div className={cardStyles.containerShadowWrapper} onClick={e => {
+                let range = document.createRange();
+                range.selectNode(document.getElementById("copyTarget"));
+                window.getSelection().removeAllRanges();
+                window.getSelection().addRange(range);
+                document.execCommand("copy");
+                window.getSelection().removeAllRanges();
+            }}>
                 <div className={cardStyles.containerShadow}/>
                 <div className={cardStyles.container}>
-                    <div className={`${textStyles.medium}`}>
+                    <div className={`${textStyles.medium} ${textStyles.bold}`}>
                         {title}
+                    </div>
+                    <div className={`${textStyles.small} ${textStyles.faint}`}>
+                        (Click to copy)
                     </div>
                     <div className={`${textStyles.mono} ${textStyles.small}`} id={"copyTarget"} style={{overflowWrap: 'anywhere', width: '90%'}}>
                         {content}
