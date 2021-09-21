f7fef5e889af1122279c467175e7e4cdd0dffdb5
qbxt <10813JJ@gmail.com>
2021-09-21 13:53:55 -0500

    update redirects for /t /i /f

--- pages/f/[filename].js
+++ pages/f/[filename].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/dist/client/router"
 
 export async function getServerSideProps(ctx) {
+    const { filename } = ctx.query;
     return {
         redirect: {
             destination: `https://storage.queue.bot/f/${filename}`,
